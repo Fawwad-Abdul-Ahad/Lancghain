543f189a5699b62e27c7178637173b06698beb61
Fawwad Abdul Ahad <fawwadabdulahad500@gmail.com>
2026-08-04 21:13:59 +0500

    update comment

--- EmbeddingModels/embedding_model_hf.py
+++ EmbeddingModels/embedding_model_hf.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 embeddings = HuggingFaceEndpointEmbeddings(
-    model="sentence-transformers/all-MiniLM-L6-v2"
+    model="sentence-transformers/all-MiniLM-L6-v2",
 )
 
 result = embeddings.embed_query("my name is fawwad")
-
+# orint results
 print(result)
