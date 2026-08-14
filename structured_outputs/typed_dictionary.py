f86e946d2d09855878d5117228e080dad8572bc2
Fawwad Abdul Ahad <fawwadabdulahad500@gmail.com>
2026-08-14 20:31:30 +0500

    structured outputs use case

--- structured_outputs/typed_dictionary.py
+++ structured_outputs/typed_dictionary.py
@@ -1,9 +1,22 @@
-from typing import TypedDict
+from typing import Literal, Optional, TypedDict, Annotated
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+# dynamic prompts
+load_dotenv()
 
-class typing (TypedDict):
-    name:str
-    age:int
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
+
+class Review (TypedDict):
+    key_themes: Annotated[list[str],"Write down all the key themes in discussed in the reiew"] 
+    summary: Annotated[str,'Write a comprehensive summary of the given review']
+    sentiment: Annotated[Literal['pos','neg'], 'Write a sentiment about the given review']
+    pros : Annotated[Optional[str], 'Write the pros of the given review']
+    cons : Annotated[Optional[str], 'Write the cons of te given review']
 
 
-new_typing = typing(name="hello", age=12)
-print(new_typing)
+structured_model = model.with_structured_output(Review)
+
+response = structured_model.invoke("I recently purchased these wireless earbuds and I am extremely impressed with the overall quality. The sound is clear, the bass is powerful, and the battery lasts much longer than I expected. I have been using them daily for work calls, music, and workouts without any issues. The charging case is compact and easy to carry. For the price, this is one of the best purchases I have made this year.")
+print(response['cons'])
